7811310cbbf4deac8a0b7b61be7b06e849769225
abdullahsakib <abdullahsakib33@gmail.com>
2025-05-13 20:09:41 +0600

    fin

--- FastKart/urls.py
+++ FastKart/urls.py
@@ -23,7 +23,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("accounts.urls")),
-    path("", include("products.urls")),
+    path("", include("products.urls"), name="home"),
     
     path("carts/", include("carts.urls")),
     path("orders/", include("orders.urls")),
